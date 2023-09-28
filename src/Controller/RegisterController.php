73bcad947c438cd4fd12b9eb9d26754cb48787f9
fxd15130 <fxd15130@gmail.com>
2023-09-28 14:54:35 +0200

    creation du registerContoller et du formtype associé

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,48 @@
 
 namespace App\Controller;
 
+Use App\Entity\User;
+use App\Form\RegisterType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'register')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, Request $request, UserPasswordHasherInterface $encoder): Response
     {
-        return $this->render('register/index.html.twig');
+
+        $notification = null;
+
+        $user = new User();
+
+        $form =$this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isSubmitted()) {
+
+            //Pour encoder un mot de passe, j'ai besoin d'injecter une dépendence. J'utilise alors UserPasswordHasherInterface
+            //je déclare un variable $password, j'utilise mon injection de dépandence $encode et jutilise un fonction qui lui est associer hashPassword
+            //Cette fonction a besoin de deux paramètre l'utilisateur($user) et le mot de passe qui lui est associer($user->getPassword)
+            $password = $encoder->hashPassword($user, $user->getPassword());
+
+
+            //Une fois que le mot de passe et encoder je le réenvoi à mon utilisateur
+            $user->setPassword($password);
+
+            //je savegarde tous en bdd
+            $userRepository->save($user);
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+            'notification' => $notification
+        ]);
     }
+    
 }
