3f75d447b3ccd0a4779d3cb21c45f22052ca753e
fxd15130 <fxd15130@gmail.com>
2023-10-01 05:58:24 +0200

    installation de EasyAdmin et nouvelle fonctionnalité

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Search;
+use App\Form\SearchType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/produits', name: 'products')]
+    public function index(ProductRepository $productRepository, Request $request): Response
+    {
+
+        $search = new Search;
+
+        $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $products = $productRepository->findWithSearch($search);
+        } else {
+            $products = $productRepository->findAll();
+        }
+
+        return $this->render('product/index.html.twig',[
+            'products' => $products,
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/produit/{slug}', name: 'product')]
+    public function show($slug, ProductRepository $productRepository): Response
+    {
+
+        $product = $productRepository->findOneBySlug($slug);
+
+        if(!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/show.html.twig',[
+              'product' => $product
+        ]);
+    }
+}
